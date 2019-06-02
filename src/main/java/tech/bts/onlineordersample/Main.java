7b1a34a78058ae7f0b52e7ff493a5e150f2d0a4f
Absolutt87 <pedrodriguez87@hotmail.com>
2019-06-02 14:40:06 +0200

    Updating Main class for printing all details of the order as required.

--- src/main/java/tech/bts/onlineordersample/Main.java
+++ src/main/java/tech/bts/onlineordersample/Main.java
@@ -1,20 +1,46 @@
 package tech.bts.onlineordersample;
 
-import java.io.*;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-
-        System.out.println("-------------Welcome to BtsMdsd-Restaurant--------------");
-        System.out.println();
-        System.out.println("# Number of orders: ");
-        System.out.println();
-        System.out.println("# All orders");
         OrdersManager ordersManager = new OrdersManager();
-        System.out.println( ordersManager.readDocument());
-    }
+        List orders = ordersManager.readOrders("src/main/resources/online-order-sample.csv");
+        List dishes = ordersManager.createDishes("src/main/resources/online-order-sample.csv");
 
+
+        System.out.println("------------------Welcome to the Restaurant-------------------" + "\n");
+        System.out.println("The total number of orders is: " + ordersManager.getNumberOrders(orders) + "\n");
+
+        System.out.println("# Order: ");
+        System.out.println("    "+ ordersManager.getOrder(orders,1));
+
+        System.out.println("# All Orders: ");
+        System.out.println(ordersManager.getAllOrdersToString(orders) + "\n" );
+
+        System.out.println("# Dish: ");
+        System.out.println(ordersManager.getDish(dishes, 0)+ "\n");
+
+        System.out.println("# All dishes: ");
+        System.out.println(ordersManager.getAllDishToString(dishes)+ "\n");
+
+        System.out.println("# Dish by Dessert type: ");
+        System.out.println(ordersManager.getDishesByType(dishes, "ds") + "\n");
+
+        System.out.println("# Dish by vgd feature: ");
+        System.out.println(ordersManager.getDishesByFeature(dishes, "vgd")+ "\n");
+
+
+        System.out.println("------------------Thanks and have a nice day!-------------------");
+
+
+
+
+
+
+
+
+    }
 }
